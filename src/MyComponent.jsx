6c72ec5d64c09442f475293e56b14d7d280ff91e
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2018-12-12 18:07:41 +0900

    Updated - fixed eslint errors and formatted by prettier

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,29 +3,31 @@ import PropTypes from 'prop-types';
 import 'rex-core/dist/css/rex-core.css';
 import './MyComponent.scss';
 
-export default function MyComponent({
-  children,
-  onClick,
-  text,
-  className
-}) {
-
+export default function MyComponent({ children, onClick, text, className }) {
   return (
-    <div className={className} onClick={onClick}>
+    <div
+      className={className}
+      onClick={onClick}
+      role="link"
+      tabIndex={0}
+      onKeyDown={onClick}
+    >
       <h1>{text}</h1>
       {children}
     </div>
   );
-};
+}
 
 MyComponent.defaultProps = {
+  children: null,
   text: 'Welcome to React',
   className: 'my-component',
-  onClick: () => { }
+  onClick: () => {},
 };
 
 MyComponent.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   text: PropTypes.string,
-  className: PropTypes.string, 
-  onClick: PropTypes.func
+  className: PropTypes.string,
+  onClick: PropTypes.func,
 };
