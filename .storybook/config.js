f1998c3a38c001a8ffdce6f3d0721f8397e0b946
Claudio <claudio.moya@rakuten.com>
2019-02-04 16:10:41 +0900

    Added - Rakuten favicon.ico from CDN, rex theme and link to repo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,19 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
+import { name, repository } from '../package.json';
+import { themes } from '@storybook/components';
+
+const rex = themes.normal;
+rex.mainBackground = '#F7F7F7';
+rex.mainTextColor = '#333333';
+rex.dimmedTextColor = '#4D4D4D';
+rex.highlightColor = '#BF0000';
 
 addDecorator(
   withOptions({
-    name: 'ReX Storybook',
-    url: 'https://github.com/rakuten-frontend'
+    name: name,
+    url: repository.url,
+    theme: rex,
   })
 );
 
