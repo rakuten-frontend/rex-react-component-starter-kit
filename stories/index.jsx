a817fb449ec6799d32a658bdd291ed3d077b5a8e
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2019-05-23 09:49:03 +0900

    Added carousel components and swipe function

--- stories/index.jsx
+++ stories/index.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withInspectHtml } from 'storybook-inspecthtml';
 import CenterDecorator from '../.storybook/centerDecorator';
@@ -19,27 +18,107 @@ stories.addDecorator(CenterDecorator);
 stories.addDecorator(checkA11y);
 stories.addDecorator(withKnobs);
 
+const items = [
+  {
+    id: 0,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item A',
+  },
+  {
+    id: 1,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item B',
+  },
+  {
+    id: 2,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item C',
+  },
+  {
+    id: 3,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item D',
+  },
+  {
+    id: 4,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item E',
+  },
+  {
+    id: 5,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item F',
+  },
+  {
+    id: 6,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item G',
+  },
+  {
+    id: 7,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item H',
+  },
+  {
+    id: 8,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item I',
+  },
+  {
+    id: 9,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item J',
+  },
+  {
+    id: 10,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item K',
+  },
+  {
+    id: 11,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item L',
+  },
+  {
+    id: 12,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item M',
+  },
+  {
+    id: 13,
+    src: 'https://picsum.photos/376/200',
+    title: 'Sagano Bamboo Forest',
+    caption: 'Located in Kyoto, Japan',
+    tabName: 'Item N',
+  },
+];
+
 // Stories
-stories.add('default', () => <Carousel />);
-stories.add('with text', () => <Carousel text="Welcome to React example" />);
-stories.add('with className', () => (
-  <Carousel className="color-black active" />
-));
-
-stories.add('with onClick', () => {
-  const onClickSample = action('clicked');
-
-  return <Carousel onClick={onClickSample} />;
-});
-
-stories.add('with children', () => (
-  <Carousel>
-    <p>Hello World</p>
-  </Carousel>
-));
-
-stories.add('with dynamic props', () => {
-  const textWelcome = text('text', 'Welcome to Dynamic React');
-
-  return <Carousel text={textWelcome} />;
-});
+stories.add('default Carousel', () => <Carousel items={items} />);
+stories.add('withTabs Carousel', () => <Carousel items={items} isTabActive />);
